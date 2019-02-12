46f893949465136f73f84591ca7f6cf7ab53c343
Acceptedlc <Accepted@icloud.com>
2019-02-12 11:18:23 +0800

    sdfdsf

--- kafka-test/app.js
+++ kafka-test/app.js
@@ -1,35 +1,7 @@
-var kafka = require('kafka-node');
-
-
-
-// const client = new kafka.KafkaClient({kafkaHost: '172.22.6.7:9092'});
-// const admin = new kafka.Admin(client);
-// admin.listTopics((err, res) => {
-//   console.log(res[1]);
-// });
-
-
-
-
-var options = {
-  kafkaHost: '172.22.6.7:9092',
-  groupId: 'mapd-test',
-  sessionTimeout: 15000,
-  protocol: ['roundrobin'],
-  encoding: 'utf8',
-
-  fromOffset: 'latest', // default
-  commitOffsetsOnFirstJoin: true,
-  outOfRangeOffset: 'earliest', // default
-  migrateHLC: false,
-  migrateRolling: true,
-};
-
-
-
-var consumerGroup = new kafka.ConsumerGroup(options, 'iot-prod');
+const {Consumer} = require('@sensoro/libkafka');
 const Connector = require("../node-connector.js");
 
+let index = 1;
 const connector = new Connector();
 connector
   .protocol("http")
@@ -39,88 +11,42 @@ connector
   .user("mapd")
   .password("HyperInteractive")
   .connect((err, con) => {
-    consumerGroup.on('connect', function () {
-      console.log("connect success");
-      consumerGroup.on('message', function (message) {
+    console.log("connect mapd success");
+    const consumer = new Consumer({
+      kafkaHost: '172.22.6.7:9092',
+      // or you can specify a zookeeperUri
+      // zookeeperUri: 'zookeeper-01:2181,zookeeper-02:2181,zookeeper-03:2181/path/to/kafka'
+      groupId: 'mapd-test',
+      topic: 'iot-prod',
+
+      // 消费3秒不结束认为超时，可选
+      consumeTimeout: 3000,
+
+      // 最多并发消费16条，可选
+      maxConsumeConcurrency: 16,
+
+
+      // 消息处理回调函数
+      messageConsumer: async (message) => {
+        console.log("recieve message", index++);
         let info = JSON.parse(message.value);
-        let log = info.message.split('\n').map(e => e.split('|')[1]).join('').replace(/\'/g,`"`);
+        let log = info.message.split('\n').map(e => e.split('|')[1]).join('').replace(/\'/g, `"`).substr(0, 100);
+
+        // await new Promise()
+
         con.query(`INSERT INTO logs VALUES ('${Math.floor(Date.now() / 1000)}', '${log}');`, {}, function (err, result) {
-          if(err) {
+          if (err) {
             console.log(`INSERT INTO logs VALUES ('${Math.floor(Date.now() / 1000)}', '${JSON.stringify(message.value)}');`);
             console.log(err);
             process.exit(1);
           }
         });
-      });
-
+      },
+      // 可选的消费失败的消息处理函数, 当 messageConsumer 发生异常或消费超时时调用
+      failedMessageConsumer: async (error, message) => {
+        console.error(error);
+      }
     });
+    consumer.listen();
   });
 
-
-
-
-
-/**
- * create table
- * inster table
- */
-
-
-// commitOffsetsOnFirstJoin: true, // on the very first time this consumer group subscribes to a topic, record the offset returned in fromOffset (latest/earliest)
-// how to recover from OutOfRangeOffset error (where save offset is past server retention) accepts same value as fromOffset
-
-// consumerGroup.on('error', function (err) {
-//   console.log(err)
-// });
-// 983381645
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var kafka = require('kafka-node'),
-//   Consumer = kafka.Consumer,
-//   client = new kafka.KafkaClient({kafkaHost: '172.22.6.7:9092'}),
-//   consumer = new Consumer(
-//     client,
-//     [
-//       {offset: 0, topic: 'iot-prod', partition: 0},
-//       {offset: 0, topic: 'iot-prod', partition: 1},
-//       {offset: 0, topic: 'iot-prod', partition: 2},
-//       {offset: 0, topic: 'iot-prod', partition: 3},
-//       {offset: 0, topic: 'iot-prod', partition: 4},
-//       {offset: 0, topic: 'iot-prod', partition: 5},
-//       {offset: 0, topic: 'iot-prod', partition: 6},
-//       {offset: 0, topic: 'iot-prod', partition: 7},
-//       {offset: 0, topic: 'iot-prod', partition: 8},
-//       {offset: 0, topic: 'iot-prod', partition: 9},
-//       {offset: 0, topic: 'iot-prod', partition: 10},
-//       {offset: 0, topic: 'iot-prod', partition: 11},
-//     ],
-//     {
-//       autoCommit: true,
-//       groupId: 'com',
-//       fetchMaxWaitMs: 100,
-//       fetchMinBytes: 1,
-//     }
-//   );
-// consumer.on('message', function (message) {
-//   console.log(message);
-// });
-//
-// consumer.on('error', function (err) {
-//   console.log(err)
-// });
